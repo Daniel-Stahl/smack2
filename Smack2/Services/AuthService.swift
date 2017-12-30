e34bf2f42747b1f0e482093a28d92fb6329fbd20
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2017-12-29 16:50:29 -0800

    logged in user

--- Smack2/Services/AuthService.swift
+++ Smack2/Services/AuthService.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
-    
+
     static let instance = AuthService()
-    
+
     let defaults = UserDefaults.standard
-    
+
     var isLoggedIn : Bool {
         get {
             return defaults.bool(forKey: LOGGED_IN_KEY)
@@ -23,7 +24,7 @@ class AuthService {
             defaults.set(newValue, forKey: LOGGED_IN_KEY)
         }
     }
-    
+
     var authToken : String {
         get {
             return defaults.value(forKey: TOKEN_KEY) as! String
@@ -32,7 +33,7 @@ class AuthService {
             defaults.set(newValue, forKey: TOKEN_KEY)
         }
     }
-    
+
     var userEmail : String {
         get {
             return defaults.value(forKey: USER_EMAIL) as! String
@@ -41,21 +42,17 @@ class AuthService {
             defaults.set(newValue, forKey: USER_EMAIL)
         }
     }
-    
+
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
-        
+
         let lowerCaseEmail = email.lowercased()
-        
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
-        ]
-        
+
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
-        
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
             } else {
@@ -63,19 +60,30 @@ class AuthService {
                 debugPrint(response.result.error as Any)
             }
         }
-        
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                let json = JSON(data: data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
 }
+
