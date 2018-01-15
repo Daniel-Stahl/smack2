bb93ea6a053f99a951439b982b397038c7f59b84
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2018-01-15 15:59:35 -0800

    working on channels

---
+++ Smack2/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack2
+//
+//  Created by Daniel Stahl on 1/15/18.
+//  Copyright © 2018 Daniel Stahl. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
--- Smack2/Utilities/Constants.swift
+++ Smack2/Utilities/Constants.swift
@@ -16,6 +16,7 @@ let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
+let URL_GET_CHANNELS = "\(BASE_URL)channel"
 
 // Colors
 let smackPurplePalceHolder = #colorLiteral(red: 0.3147586286, green: 0.5386250615, blue: 1, alpha: 0.5)
