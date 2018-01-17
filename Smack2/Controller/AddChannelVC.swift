7fbe725db028b53be4489da039d3a2af727e4b83
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2018-01-16 16:07:51 -0800

    add channel model works

---
+++ Smack2/Controller/AddChannelVC.swift
@@ -0,0 +1,47 @@
+//
+//  AddChannelVC.swift
+//  Smack2
+//
+//  Created by Daniel Stahl on 1/16/18.
+//  Copyright © 2018 Daniel Stahl. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var descTxt: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    func setupview() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "Name", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePalceHolder])
+        
+        descTxt.attributedPlaceholder = NSAttributedString(string: "Description", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePalceHolder])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
